8804a71e37922929d5931e910632f729ffab8096
Alfredoeb9 <alfredoeb96@gmail.com>
2023-12-09 20:54:24 -0800

    [initial] initial next config

---
+++ src/app/match-finder/cash-matches/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function CashMatches() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            from CashMatches, test
+        </div>
+    )
+}
