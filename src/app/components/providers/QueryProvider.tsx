cb637b2605e885c08ce4585d1479b085cc705f4c
Alfredoeb9 <alfredoeb96@gmail.com>
2024-01-29 20:42:20 -0800

    [react-query] implemented react-query devtools and fixed tsconfig from node to nodenext

--- src/app/components/providers/QueryProvider.tsx
+++ src/app/components/providers/QueryProvider.tsx
@@ -3,6 +3,7 @@ import {
     QueryClient,
     QueryClientProvider,
 } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const QueryProvider = ({ children }: { children: React.ReactNode }) => {
     const queryClient = new QueryClient();
@@ -10,6 +11,7 @@ const QueryProvider = ({ children }: { children: React.ReactNode }) => {
     return (
         <QueryClientProvider client={queryClient}>    
             {children}
+            <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
     )
 }
