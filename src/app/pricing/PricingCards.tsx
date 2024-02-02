a757e0d844ee363c5087c09b4980f58789f08483
Alfredoeb9 <alfredoeb96@gmail.com>
2024-02-02 00:24:13 -0800

    [css] updated css for pricing card ui

--- src/app/pricing/PricingCards.tsx
+++ src/app/pricing/PricingCards.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@nextui-org/react";
 import React from "react";
+import { Card, CardBody, CardFooter } from "@nextui-org/react";
 import { checkoutAction } from "./actions";
 import getStripe from "utils/get-stripejs";
 import { pricingCards } from "@/lib/PricingCards";
@@ -11,33 +12,39 @@ export default function PricingCards() {
 
     return (
         <div>
-            <div className="flex flex-row items-center justify-between">
+            <div className="gap-5 grid grid-cols-2 sm:grid-cols-4">
                 {pricingCards.map((card: any) => (
-                    <div className="">
-                        <Image src={"/images/mw3.png"} width={60} height={60} alt={""} />
-                        <p>{card.credits} Credits</p>
+                    <Card key={card.id} isPressable onPress={() => {
+                        checkoutAction(card.credits).then(async (session) => {
+                            const stripe = await getStripe();
+                            if (stripe === null) return;
+                            await stripe!.redirectToCheckout({
+                                sessionId: session.id,
+                            });
+                        }).catch(() => {
+                            console.log("log in hawmie")
+                            // toast({
+                            //     variat: "destructive",
+                            //     title: "Something went wrong",
+                            //     descripition: "You much be loggin in to buy credits"
+                            // })
+                        })
+                    }}>
+                        <CardBody className="flex justify-center items-center">
+                            <Image src={"/images/mw3.png"} width={170} height={170} alt={""} />
+                            
+                            
+                        </CardBody>
+                        
 
-                        <Button
-                            onClick={() => {
-                                checkoutAction(card.credits).then(async (session) => {
-                                    const stripe = await getStripe();
-                                    if (stripe === null) return;
-                                    await stripe!.redirectToCheckout({
-                                        sessionId: session.id,
-                                    });
-                                }).catch(() => {
-                                    console.log("log in hawmie")
-                                    // toast({
-                                    //     variat: "destructive",
-                                    //     title: "Something went wrong",
-                                    //     descripition: "You much be loggin in to buy credits"
-                                    // })
-                                })
-                            }}
-                        >
-                            Buy for ${card.cost}
-                        </Button>
-                    </div>
+                        <CardFooter className="flex justify-center items-center">
+                                <p className="mr-2">{card.credits} Credits</p>
+
+                                <span className="bg-slate-400 px-3 py-4 rounded-md text-white hover:scale-105 shadow-xl">
+                                    Buy for ${card.cost}
+                                </span>
+                        </CardFooter>
+                    </Card>
                 ))}
             </div>
 
--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,13 +1,27 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import PricingCards from "./PricingCards";
+import { useSession } from "next-auth/react";
+import AccessDenied from "../components/access-denied";
+import { useRouter } from "next/navigation";
 
 export default function Pricing() {
+    const { data: user, status } = useSession();
+    const router = useRouter()
+
+    // console.log("session", session)
+
+    // status could === 
+    // unauthenticated || authenticated
+
+    if (!user || status === 'unauthenticated') return router.push('/auth/sign-in')
+
     return (
-        <section>
-            Buy Credits!
-            <p>Purchase credits to enter into tournaments or cash matches</p>
-            <p>Please review our <Link href={"/refund-policy"}>Refund Policy</Link> before buying credits. We do not issue refunds at this time.</p>
+        <section className="flex justify-center items-center flex-col max-w-7xl w-full p-8">
+            <h1 className="text-3xl mb-2">Buy Credits!</h1>
+            <p className="mb-2">Purchase credits to enter into tournaments or cash matches</p>
+            <p className="mb-2">Please review our <Link href={"/refund-policy"}>Refund Policy</Link> before buying credits. We do not issue refunds at this time.</p>
             <PricingCards />
         </section>
     )
